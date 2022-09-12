7875e0906ff7641b17e7ec7da8b0f73d6af6b8cc
Wiktoria Sowa <Wiktoria.Sowa@bshg.com>
2022-09-12 11:20:54 +0200

    Add mui, rename app, move app files

---
+++ client/src/App/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import SudokuBoard from "../components/SudokuBoard";
+
+function App() {
+  return (
+    <div className="App">
+      <SudokuBoard />
+    </div>
+  );
+}
+
+export default App;
